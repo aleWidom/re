e4d03b05f0c9babd0a4613c2a28857bb32f2491d
alejandro <widomlanski@hotmail.com>
2024-03-08 16:56:03 -0300

    search parmams search

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,23 @@
+/* import type {NextRequest} from "next/server";
+
+import {revalidatePath} from "next/cache";
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path") || "/";
+
+  revalidatePath(path);
+
+  return Response.json({success: true});
+} */
+
+import type {NextRequest} from "next/server";
+
+import {revalidateTag} from "next/cache";
+
+export async function GET(request: NextRequest) {
+  const tag = request.nextUrl.searchParams.get("tag") || "restaurants";
+
+  revalidateTag(tag);
+
+  return Response.json({success: true});
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
-export default function HomePage() {
-  return <section>👋</section>;
+import Link from "next/link";
+
+import api from "@/api";
+
+import SearchBox from "./components/SearchBox";
+
+export default async function Home({searchParams}: {searchParams: {query: string}}) {
+  const restaurants = await api.search(searchParams.query);
+  /*   const restaurants = await api.list(); */
+
+  return (
+    <section>
+      <SearchBox />
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+        {restaurants.map((restaurant) => {
+          return (
+            <article key={restaurant.id}>
+              <img
+                alt={restaurant.name}
+                className="mb-3 h-[300px] w-full object-cover"
+                src={restaurant.image}
+              />
+              <h2 className="inline-flex gap-2 text-lg font-bold">
+                <Link key={restaurant.id} href={`/${restaurant.id}`}>
+                  {restaurant.name}
+                </Link>
+                <small className="inline-flex gap-1">
+                  <span>⭐</span>
+                  <span>{restaurant.score}</span>
+                  <span className="font-normal opacity-75">({restaurant.ratings})</span>
+                </small>
+              </h2>
+              <p className="opacity-90">{restaurant.description}</p>
+            </article>
+          );
+        })}
+      </section>
+    </section>
+  );
 }
